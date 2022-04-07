5474089a74f5ebfa1988ca93ddae2648e85f1545
nurmdrafi <nurmdrafi2014@gmail.com>
2022-04-07 21:07:31 +0600

    firebase init

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,13 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDKMHVYHg-G_SSbMmVxDCvdZeiHDdw1BxI",
-  authDomain: "fir-authentication-596ac.firebaseapp.com",
-  projectId: "fir-authentication-596ac",
-  storageBucket: "fir-authentication-596ac.appspot.com",
-  messagingSenderId: "513568426470",
-  appId: "1:513568426470:web:d9656c496ff95a53382e27"
+  apiKey: "AIzaSyCNYZJuETouV5Sef1chiv6_60vCDNaR8dA",
+  authDomain: "email-password-auth-eacbf.firebaseapp.com",
+  projectId: "email-password-auth-eacbf",
+  storageBucket: "email-password-auth-eacbf.appspot.com",
+  messagingSenderId: "672391335313",
+  appId: "1:672391335313:web:b5e1954a48c5e6590ede30"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
